87c28553143c2ea9c2c82a92a0cdeb5198a79a99
Jonathan Senkerik <josenk@jintegrate.co>
2018-05-31 07:36:54 -0400

    init commit

--- esxi/guest_create.go
+++ esxi/guest_create.go
@@ -5,15 +5,16 @@ import (
 	"fmt"
 	"log"
 	"os/exec"
-	//"golang.org/x/crypto/ssh"
-	//"strconv"
 	"strings"
 )
+
 
 func GuestCreate(esxi_hostname string, esxi_hostport string, esxi_username string, encoded_esxi_password string,
 	 guest_name string, esxi_disk_store string, src_path string, esxi_resource_pool string) (string, int) {
 
-	dst_path := fmt.Sprintf("vi://%s:%s@%s/%s", esxi_username, encoded_esxi_password, esxi_hostname, esxi_resource_pool)
+  esxiSSHinfo := SshConnectionInfo{esxi_hostname, esxi_hostport, esxi_username, encoded_esxi_password}
+
+	dst_path := fmt.Sprintf("vi://%s:%s@%s/%s", esxiSSHinfo.user, esxiSSHinfo.pass, esxiSSHinfo.host, esxi_resource_pool)
 
 	ovf_cmd := fmt.Sprintf("ovftool --acceptAllEulas --noSSLVerify --X:useMacNaming=false " +
 		"-dm=thin --name='%s' --overwrite -ds='%s' '%s' '%s'",guest_name, esxi_disk_store, src_path, dst_path)
@@ -33,29 +34,18 @@ func GuestCreate(esxi_hostname string, esxi_hostport string, esxi_username strin
 		return "There was an ovftool Error",1
 	}
 
-	esxi_hostandport := fmt.Sprintf("%s:%s", esxi_hostname, esxi_hostport)
-	client, session, err := connectToHost(esxi_username, encoded_esxi_password, esxi_hostandport)
-	if err != nil {
-		log.Println("[provider-esxi] Failed to ssh to esxi host: " + err.Error())
-		return "Failed to ssh to esxi host",1
-  }
-
-
-
   remote_cmd := fmt.Sprintf("vim-cmd vmsvc/getallvms 2>/dev/null | sort -n | " +
 		"grep \"[0-9] * %s .*%s\" | awk '{print $1}' | " +
 		"tail -1", guest_name, guest_name)
 
-	stdout, err := session.CombinedOutput(remote_cmd)
+  stdout, err := runRemoteSshCommand(esxiSSHinfo, remote_cmd, "get vmid")
 	vmid := strings.TrimSpace(string(stdout))
 	log.Printf("[provider-esxi] get_vmid_cmd: %s", vmid)
 	if err != nil {
-		log.Println("[provider-esxi] There was an Error getting vmid: " + err.Error())
-		return "There was an Error getting vmid",1
+		log.Printf("[provider-esxi] Failed get vmid_cmd: %s", stdout)
+		return "Failed", 1
 	}
 
 
-
-	client.Close()
-  return vmid, 0
+  return vmid,0
 }
--- esxi/guest_delete.go
+++ esxi/guest_delete.go
@@ -3,33 +3,20 @@ package esxi
 import (
 	"fmt"
 	"log"
-	//"strconv"
-	//"strings"
 )
 
-func GuestDelete(esxi_hostname string, esxi_hostport string, esxi_username string, esxi_password string,
-  vmid string) int {
-    encoded_esxi_password := esxi_password
 
-    esxi_hostandport := fmt.Sprintf("%s:%s", esxi_hostname, esxi_hostport)
-  	client, session, err := connectToHost(esxi_username, encoded_esxi_password, esxi_hostandport)
-  	if err != nil {
-  		log.Println("[provider-esxi] Failed to ssh to esxi host: " + err.Error())
-  		return 1
-    }
+func GuestDelete(esxi_hostname string, esxi_hostport string, esxi_username string, encoded_esxi_password string,
+	 vmid string) int {
 
+    esxiSSHinfo := SshConnectionInfo{esxi_hostname, esxi_hostport, esxi_username, encoded_esxi_password}
 
     remote_cmd := fmt.Sprintf("vim-cmd vmsvc/destroy %s", vmid)
-  	stdout, err := session.CombinedOutput(remote_cmd)
-  	//vmid := strings.TrimSpace(string(stdout))
-  	log.Printf("[provider-esxi] guest destroy stdout: %s", stdout)
-  	if err != nil {
-  		log.Println("[provider-esxi] There was an Error destroying vmid: " + err.Error())
-  		return 1
-  	}
 
-
-
-  	client.Close()
+		stdout, err := runRemoteSshCommand(esxiSSHinfo, remote_cmd, "get vmid")
+		if err != nil {
+			log.Printf("[provider-esxi] Failed destroy vmid: %s", stdout)
+			return 1
+		}
     return 0
   }
