850490adbc5c3ac7086ee2f2ad6ba397d262dd68
Dave (Dizzy) Smith <dizzyd@dizzyd.com>
2022-10-11 10:31:46 -0600

    Fix logging to work properly with latest terraform

--- esxi/provider.go
+++ esxi/provider.go
@@ -1,19 +1,9 @@
 package esxi
 
 import (
-	"fmt"
-	"log"
-	"os"
-
 	"github.com/hashicorp/terraform/helper/schema"
 	"github.com/hashicorp/terraform/terraform"
 )
-
-func init() {
-	// Terraform is already adding the timestamp for us
-	log.SetFlags(log.Lshortfile)
-	log.SetPrefix(fmt.Sprintf("pid-%d-", os.Getpid()))
-}
 
 func Provider() terraform.ResourceProvider {
 	return &schema.Provider{
